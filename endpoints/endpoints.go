9340210ebe62bed7a0667585da13f11d5cbd3348
luantranminh <15520454@gm.uit.edu.vn>
2018-07-24 18:17:37 +0700

    CRUD for category
    - create category model
    - create migration
    - create service
    - create endpoint
    - create decoder

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-kit/kit/endpoint"
 	"github.com/luantranminh/example-go/service"
 
+	"github.com/luantranminh/example-go/endpoints/category"
 	"github.com/luantranminh/example-go/endpoints/user"
 )
 
@@ -14,15 +15,26 @@ type Endpoints struct {
 	CreateUser  endpoint.Endpoint
 	UpdateUser  endpoint.Endpoint
 	DeleteUser  endpoint.Endpoint
+
+	FindCategory    endpoint.Endpoint
+	FindAllCategory endpoint.Endpoint
+	CreateCategory  endpoint.Endpoint
+	UpdateCategory  endpoint.Endpoint
+	DeleteCategory  endpoint.Endpoint
 }
 
 // MakeServerEndpoints returns an Endpoints struct
 func MakeServerEndpoints(s service.Service) Endpoints {
 	return Endpoints{
-		FindUser:    user.MakeFindEndPoint(s),
-		FindAllUser: user.MakeFindAllEndpoint(s),
-		CreateUser:  user.MakeCreateEndpoint(s),
-		UpdateUser:  user.MakeUpdateEndpoint(s),
-		DeleteUser:  user.MakeDeleteEndpoint(s),
+		FindUser:        user.MakeFindEndPoint(s),
+		FindAllUser:     user.MakeFindAllEndpoint(s),
+		CreateUser:      user.MakeCreateEndpoint(s),
+		UpdateUser:      user.MakeUpdateEndpoint(s),
+		DeleteUser:      user.MakeDeleteEndpoint(s),
+		FindCategory:    category.MakeFindEndPoint(s),
+		FindAllCategory: category.MakeFindAllEndpoint(s),
+		CreateCategory:  category.MakeCreateEndpoint(s),
+		UpdateCategory:  category.MakeUpdateEndpoint(s),
+		DeleteCategory:  category.MakeDeleteEndpoint(s),
 	}
 }
