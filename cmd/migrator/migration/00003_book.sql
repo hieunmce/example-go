38b3a9144cb584bcb0402599d0368e2d7f66d3f0
luantranminh <15520454@gm.uit.edu.vn>
2018-07-25 13:23:40 +0700

    Add book.sql

--- cmd/migrator/migration/00003_book.sql
+++ cmd/migrator/migration/00003_book.sql
@@ -1,13 +1,16 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE TABLE "public"."categories" (
+CREATE TABLE "public"."books" (
   "id" uuid NOT NULL,
   "created_at" timestamptz DEFAULT now(),
   "deleted_at" timestamptz,
   "name" text,
-  CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+  "author" text,
+  "descripsion" text,
+  "category_id" uuid references categories,
+  CONSTRAINT "books_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
 
-DROP TABLE categories
+DROP TABLE "public"."books"
