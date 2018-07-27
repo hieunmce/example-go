5ebc8f8e8beee663acb2f3ce095324d53ea7c139
kimtt <trantrongkim98@gmail.com>
2018-07-27 10:58:12 +0700

    finished service.go for lendbook

--- service/service.go
+++ service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/trantrongkim98/example-go/service/book"
 	"github.com/trantrongkim98/example-go/service/category"
+	"github.com/trantrongkim98/example-go/service/lendbook"
 	"github.com/trantrongkim98/example-go/service/user"
 )
 
@@ -11,4 +12,5 @@ type Service struct {
 	UserService     user.Service
 	CategoryService category.Service
 	BookService     book.Service
+	LendBookService lendbook.Service
 }
