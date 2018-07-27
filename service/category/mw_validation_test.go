00d0fd1c6b0c60a36097b5a219df68dd5655874d
luantranminh <15520454@gm.uit.edu.vn>
2018-07-27 07:36:19 +0700

    Modify validation test for category
    - Add more test case for validationMiddleware_Create
    - Adjust validationMiddleware_Update
    - Change type of wantErr to erro from bool

--- service/category/mw_validation_test.go
+++ service/category/mw_validation_test.go
@@ -24,7 +24,7 @@ func Test_validationMiddleware_Create(t *testing.T) {
 	tests := []struct {
 		name            string
 		args            args
-		wantErr         bool
+		wantErr         error
 		errorStatusCode int
 	}{
 		{
@@ -32,13 +32,21 @@ func Test_validationMiddleware_Create(t *testing.T) {
 			args: args{&domain.Category{
 				Name: "this is a valid category",
 			}},
-			wantErr:         true,
+			wantErr:         nil,
 			errorStatusCode: http.StatusBadRequest,
 		},
 		{
 			name:            "invalid category by missing name",
 			args:            args{&domain.Category{}},
-			wantErr:         true,
+			wantErr:         ErrNameIsRequired,
+			errorStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid category by name'length < 5",
+			args: args{&domain.Category{
+				Name: "a",
+			}},
+			wantErr:         ErrNameTooShort,
 			errorStatusCode: http.StatusBadRequest,
 		},
 	}
@@ -49,7 +57,7 @@ func Test_validationMiddleware_Create(t *testing.T) {
 			}
 			err := mw.Create(defaultCtx, tt.args.p)
 			if err != nil {
-				if tt.wantErr == false {
+				if tt.wantErr != err {
 					t.Errorf("validationMiddleware.Create() error = %v, wantErr %v", err, tt.wantErr)
 					return
 				}
@@ -137,34 +145,63 @@ func Test_validationMiddleware_Find(t *testing.T) {
 }
 
 func Test_validationMiddleware_Update(t *testing.T) {
-	type fields struct {
-		Service Service
+	serviceMock := &ServiceMock{
+		UpdateFunc: func(_ context.Context, p *domain.Category) (*domain.Category, error) {
+			return p, nil
+		},
 	}
+
+	defaultCtx := context.Background()
 	type args struct {
-		ctx      context.Context
 		category *domain.Category
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *domain.Category
-		wantErr bool
+		name            string
+		args            args
+		wantOutput      *domain.Category
+		wantErr         error
+		errorStatusCode int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "valid user",
+			args: args{&domain.Category{
+				Name: "Nameisvalid",
+			}},
+			wantOutput: &domain.Category{
+				Name: "Nameisvalid",
+			},
+		},
+		{
+			name:            "invalid category by missing name",
+			args:            args{&domain.Category{}},
+			wantErr:         ErrNameIsRequired,
+			errorStatusCode: http.StatusBadRequest,
+		},
+
+		{
+			name: "invalid category by new name to short",
+			args: args{&domain.Category{
+				Name: "No",
+			}},
+			wantErr:         ErrNameTooShort,
+			errorStatusCode: http.StatusBadRequest,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mw := validationMiddleware{
-				Service: tt.fields.Service,
+				Service: serviceMock,
 			}
-			got, err := mw.Update(tt.args.ctx, tt.args.category)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("validationMiddleware.Update() error = %v, wantErr %v", err, tt.wantErr)
-				return
+
+			got, err := mw.Update(defaultCtx, tt.args.category)
+			if err != nil {
+				if tt.wantErr != err {
+					t.Errorf("validationMiddleware.Create() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("validationMiddleware.Update() = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(got, tt.wantOutput) {
+				t.Errorf("validationMiddleware.Update() = %v, want %v", got, tt.wantOutput)
 			}
 		})
 	}
