106a460990babb19eafa2bc7d2fcf3dd6e0bcca4
luquehuong <luquehuong@gmail.com>
2018-08-02 18:30:09 +0700

    complete endpoints

--- service/service.go
+++ service/service.go
@@ -2,9 +2,11 @@ package service
 
 import "github.com/luquehuong/example-go/service/user"
 import "github.com/luquehuong/example-go/service/category"
+import "github.com/luquehuong/example-go/service/book"
 
 // Service define list of all services in projects
 type Service struct {
 	UserService     user.Service
 	CategoryService category.Service
+	BookService     book.Service
 }
