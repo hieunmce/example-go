d4e822b3b6219f8e509ba8119dceb6f501b1d77e
luquehuong <luquehuong@gmail.com>
2018-08-07 14:29:00 +0700

    test if name of category is already existed when update a category

--- service/category/pg_test.go
+++ service/category/pg_test.go
@@ -79,34 +79,73 @@ func TestPGService_Create(t *testing.T) {
 }
 
 func Test_pgService_Update(t *testing.T) {
-	type fields struct {
-		db *gorm.DB
+	t.Parallel()
+	testDB, _, cleanup := testutil.CreateTestDatabase(t)
+	defer cleanup()
+	err := testutil.MigrateTables(testDB)
+	if err != nil {
+		t.Fatalf("Failed to migrate table by error %v", err)
 	}
+
+	category := domain.Category{}
+	err = testDB.Create(&category).Error
+	if err != nil {
+		t.Fatalf("Failed to create category by error %v", err)
+	}
+
+	fakeCategoryID := domain.MustGetUUIDFromString("1698bbd6-e0c8-4957-a5a9-8c536970994b")
+
 	type args struct {
-		in0 context.Context
-		p   *domain.Category
+		p *domain.Category
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
-		want    *domain.Category
-		wantErr bool
+		wantErr error
 	}{
 		// TODO: Add test cases.
+		{
+			name: "success update",
+			args: args{
+				&domain.Category{
+					Model: domain.Model{ID: category.ID},
+					Name:  "category Name 1",
+				},
+			},
+		},
+		{
+			name: "failed update",
+			args: args{
+				&domain.Category{
+					Model: domain.Model{ID: fakeCategoryID},
+					Name:  "category Name 1",
+				},
+			},
+			wantErr: ErrNotFound,
+		},
+		{
+			name: "success update",
+			args: args{
+				&domain.Category{
+					Model: domain.Model{ID: category.ID},
+					Name:  "category Name 1",
+				},
+			},
+			wantErr: ErrNameIsExisted,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &pgService{
-				db: tt.fields.db,
+				db: testDB,
 			}
-			got, err := s.Update(tt.args.in0, tt.args.p)
-			if (err != nil) != tt.wantErr {
+			_, err := s.Update(context.Background(), tt.args.p)
+			if err != nil && err != tt.wantErr {
 				t.Errorf("pgService.Update() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("pgService.Update() = %v, want %v", got, tt.want)
+			if err == nil && tt.wantErr != nil {
+				t.Errorf("pgService.Update() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
