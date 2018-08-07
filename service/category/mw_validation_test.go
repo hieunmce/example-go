fef76fb1253065ed22e8758fd38bc3d1b9e02ee4
luquehuong <luquehuong@gmail.com>
2018-08-07 11:35:11 +0700

    test validate name of category is not empty and lenght name > 5 characters [in update category]

--- service/category/mw_validation_test.go
+++ service/category/mw_validation_test.go
@@ -2,27 +2,12 @@ package category
 
 import (
 	"context"
+	"net/http"
 	"reflect"
 	"testing"
 
 	"github.com/luquehuong/example-go/domain"
 )
-
-func TestValidationMiddleware(t *testing.T) {
-	tests := []struct {
-		name string
-		want func(Service) Service
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ValidationMiddleware(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ValidationMiddleware() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func Test_validationMiddleware_Create(t *testing.T) {
 	type fields struct {
@@ -124,30 +109,114 @@ func Test_validationMiddleware_Update(t *testing.T) {
 		Service Service
 	}
 	type args struct {
-		ctx      context.Context
-		category *domain.Category
+		p *domain.Category
 	}
+	serviceMock := &ServiceMock{
+		UpdateFunc: func(_ context.Context, p *domain.Category) (*domain.Category, error) {
+			return p, nil
+		},
+	}
+
+	defaultCtx := context.Background()
+
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *domain.Category
-		wantErr bool
+		name            string
+		args            args
+		wantOutput      *domain.Category
+		wantErr         bool
+		errorStatusCode int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "valid user",
+			args: args{&domain.Category{
+				Name: "Curabitur vulputate vestibulum lorem.",
+			}},
+			wantOutput: &domain.Category{
+				Name: "Curabitur vulputate vestibulum lorem.",
+			},
+		},
+		{
+			name:            "invalid user by missing name",
+			args:            args{&domain.Category{}},
+			wantErr:         true,
+			errorStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid user by name is empty",
+			args: args{&domain.Category{
+				Name: "",
+			}},
+			wantErr:         true,
+			errorStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid user by lenght name < 5 characters",
+			args: args{&domain.Category{
+				Name: "a",
+			}},
+			wantErr:         true,
+			errorStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid user by lenght name < 5 characters",
+			args: args{&domain.Category{
+				Name: "ab",
+			}},
+			wantErr:         true,
+			errorStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid user by lenght name < 5 characters",
+			args: args{&domain.Category{
+				Name: "abc",
+			}},
+			wantErr:         true,
+			errorStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid user by lenght name < 5 characters",
+			args: args{&domain.Category{
+				Name: "abcd",
+			}},
+			wantErr:         true,
+			errorStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid user by lenght name < 5 characters",
+			args: args{&domain.Category{
+				Name: "abcde",
+			}},
+			wantErr:         true,
+			errorStatusCode: http.StatusBadRequest,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mw := validationMiddleware{
-				Service: tt.fields.Service,
+				Service: serviceMock,
 			}
-			got, err := mw.Update(tt.args.ctx, tt.args.category)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("validationMiddleware.Update() error = %v, wantErr %v", err, tt.wantErr)
+			gotOutput, err := mw.Update(defaultCtx, tt.args.p)
+			if err != nil {
+				if tt.wantErr == false {
+					t.Errorf("validationMiddleware.Update() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+
+				status, ok := err.(interface{ StatusCode() int })
+				if !ok {
+					t.Errorf("validationMiddleware.Update() error %v doesn't implement StatusCode()", err)
+				}
+				if tt.errorStatusCode != status.StatusCode() {
+					t.Errorf("validationMiddleware.Update() status = %v, want status code %v", status.StatusCode(), tt.errorStatusCode)
+					return
+				}
+
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("validationMiddleware.Update() = %v, want %v", got, tt.want)
+
+			if !reflect.DeepEqual(gotOutput, tt.wantOutput) {
+				t.Errorf("ValidationMiddleware.Update() = %v, want %v, err = %v", gotOutput, tt.wantOutput, err)
 			}
 		})
 	}
